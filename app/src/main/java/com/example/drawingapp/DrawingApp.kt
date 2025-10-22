24d71d83eed061852e506a6b3ebe1ce929d635cb
paolowdio <paolowd11@gmail.com>
2025-10-22 17:26:09 -0600

    added repository and singleton design

---
+++ app/src/main/java/com/example/drawingapp/DrawingApp.kt
@@ -0,0 +1,23 @@
+package com.example.drawingapp
+
+import android.app.Application
+import androidx.room.Room
+import kotlinx.coroutines.CoroutineScope
+import kotlin.getValue
+import com.example.drawingapp.storage.ImageDao
+import com.example.drawingapp.storage.ImageEntity
+import com.example.drawingapp.storage.ImageDatabase
+import kotlinx.coroutines.SupervisorJob
+
+///Singleton so only one repository is created
+class DrawingApp: Application() {
+    val scope =CoroutineScope(SupervisorJob())
+    val db by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            ImageDatabase::class.java,
+            "drawing_DB"
+        ).build()
+    }
+    val repository by lazy { ImageRepository(scope, db.imageDao()) }
+}
---
+++ app/src/main/java/com/example/drawingapp/ImageRepository.kt
@@ -0,0 +1,35 @@
+package com.example.drawingapp
+
+import android.graphics.Bitmap
+import androidx.compose.ui.graphics.ImageBitmap
+import com.example.drawingapp.storage.ImageDao
+import com.example.drawingapp.storage.ImageEntity
+import com.example.drawingapp.storage.ImageDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+import kotlin.io.use
+import android.content.Context
+
+class ImageRepository (
+    private val scope: CoroutineScope,
+    private val dao: ImageDao
+)
+{
+    val allImages : Flow<List<ImageEntity>> = dao.getAllImages()
+
+    fun saveImage(context: Context, bitmap: Bitmap){
+        scope.launch(Dispatchers.IO) {
+            val filename = "drawing_app_${System.currentTimeMillis()}.png"
+            val file = File(context.filesDir, filename)
+            file.outputStream().buffered().use { image: OutputStream ->
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, image)
+            }
+            dao.insertFile(ImageEntity(filepath = file.absolutePath))
+        }
+    }
+}
